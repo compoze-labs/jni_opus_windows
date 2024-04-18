21630aac76282bd6a1eea7ddeac21d479206f6d0
Jon Christensen <jon.christensen@compozelabs.com>
2024-04-18 10:45:00 -0500

    opus_jni c and h

---
+++ include/opus_jni.h
@@ -0,0 +1,10 @@
+#include <jni.h>
+
+#ifndef JNI_OPUS_OPUS_JNI_H
+#define JNI_OPUS_OPUS_JNI_H
+
+//int get100(void);
+//int get1000(void);
+JNIEXPORT int JNICALL Java_HelloJNI_sayHello(JNIEnv *, jobject);
+
+#endif //JNI_OPUS_OPUS_JNI_H
