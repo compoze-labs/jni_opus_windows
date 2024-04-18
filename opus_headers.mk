0eea769bc913820d6a1a9f25256400e29bbe00dd
Jon Christensen <jon.christensen@compozelabs.com>
2024-04-18 10:45:59 -0500

    overwrite .mks from macos

--- opus_headers.mk
+++ opus_headers.mk
@@ -1,7 +1,11 @@
 OPUS_HEAD = \
 include/opus.h \
+include/opus_jni.h \
 include/opus_multistream.h \
 include/opus_projection.h \
+include/jawt_md.h \
+include/jni.h \
+include/jni_md.h \
 src/opus_private.h \
 src/analysis.h \
 src/mapping_matrix.h \
